0a64e057d62bef74488c095100fb258054840fb9
ee7klt <kltah1@gmail.com>
2014-12-27 17:20:05 +0800

    read file and write contents

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ app.use(express.logger({
 }));
 
 
+var fs = require('fs');
+
 http.createServer(function(request, response) {
-	response.writeHead(200);
-	response.write("Dog is running");
-  setTimeout(function() {
-    response.write("Dog is done");
-    response.end();
-  },5000);
-	response.end();
+  response.writeHead(200);
+  fs.readFile('app.js', function(error, contents) {
+      response.write(contents);
+      response.end();
+  });
+
 }).listen(8080);
 
 console.log("listening on 8080");
